ded66b84f10fd7f2a79b1b9f663b81977da8f2cb
Nathan <nathantedla@gmail.com>
2021-10-28 01:57:54 -0700

    portfolio first stage

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The Purpose of This Portfolio is to help and aspiring my Development
+        skill to the next level and build different awesome apps.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
